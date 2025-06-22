00a822e459fd82472d6feeda94804c35dd354b12
Ibrahim Ezzine <ibrahimezzine09@outlook.com>
2025-06-22 11:19:56 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 import GameScreen from '@/components/game/GameScreen';
 
+export const metadata = {
+  title: 'Skyfall Boomer',
+  description: 'Bienvenue sur Skyfall Boomer',
+  other: {
+    'google-site-verification': '56_RypUHAdcOMV5XgJKxTnIY7IXdJPViXdR8GXKIdYc',
+  },
+};
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-game-screen">
